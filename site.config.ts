3eee968082150d70cf9addc5a242b91862defc39
Bryan Chua <me@bryanchua.com>
2026-01-02 10:30:22 +0800

    Update site.config.ts
    switch to personal notion

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Our-Photo-Hub-2dc8bddea6c9810c8193c7ef87efe040',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
